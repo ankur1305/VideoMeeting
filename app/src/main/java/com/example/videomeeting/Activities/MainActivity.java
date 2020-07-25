2ed2985aa11cc6b83f443196733818397aea85a6
Ankur singhal <44199303+ankur1305@users.noreply.github.com>
2020-07-25 19:03:15 +0530

    Display User List From Firestore Database

--- app/src/main/java/com/example/videomeeting/Activities/MainActivity.java
+++ app/src/main/java/com/example/videomeeting/Activities/MainActivity.java
@@ -1,25 +1,37 @@
 package com.example.videomeeting.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.videomeeting.Adapters.UsersAdapter;
+import com.example.videomeeting.Models.User;
 import com.example.videomeeting.R;
 import com.example.videomeeting.Utilities.Constants;
 import com.example.videomeeting.Utilities.PreferenceManager;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.iid.FirebaseInstanceId;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     PreferenceManager preferenceManager;
+    private List<User> users;
+    private UsersAdapter usersAdapter;
+    private TextView textErrorMessage;
+    private ProgressBar usersProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,24 +50,66 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.textSignOut).setOnClickListener(v -> signOut());
 
         FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(task -> {
-                if(task.isSuccessful() && task.getResult() != null) {
-                    sendFCMTokenToDatabase(task.getResult().getToken());
-                }
+            if (task.isSuccessful() && task.getResult() != null) {
+                sendFCMTokenToDatabase(task.getResult().getToken());
+            }
         });
+
+        RecyclerView usersRecyclerView = findViewById(R.id.usersRecyclerView);
+        textErrorMessage = findViewById(R.id.textErrorMessage);
+        usersProgressBar = findViewById(R.id.usersProgressBar);
+
+        users = new ArrayList<>();
+        usersAdapter = new UsersAdapter(users);
+        usersRecyclerView.setAdapter(usersAdapter);
+
+        getUsers();
     }
 
-    private void sendFCMTokenToDatabase(String token){
+    private void getUsers() {
+        usersProgressBar.setVisibility(View.VISIBLE);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .get()
+                .addOnCompleteListener(task -> {
+                    usersProgressBar.setVisibility(View.GONE);
+                    String myUserId = preferenceManager.getString(Constants.KEY_USER_ID);
+                    if (task.isSuccessful() && task.getResult() != null) {
+                        for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                            if (myUserId.equals(documentSnapshot.getId())) {
+                                continue;
+                            }
+                            User user = new User();
+                            user.firstName = documentSnapshot.getString(Constants.KEY_FIRST_NAME);
+                            user.lastName = documentSnapshot.getString(Constants.KEY_LAST_NAME);
+                            user.email = documentSnapshot.getString(Constants.KEY_EMAIL);
+                            user.token = documentSnapshot.getString(Constants.KEY_FCM_TOKEN);
+                            users.add(user);
+                        }
+                        if (users.size() > 0) {
+                            usersAdapter.notifyDataSetChanged();
+                        } else {
+                            textErrorMessage.setText(String.format("%s", "No Available Users"));
+                            textErrorMessage.setVisibility(View.VISIBLE);
+                        }
+                    } else {
+                        textErrorMessage.setText(String.format("%s", "No Available Users"));
+                        textErrorMessage.setVisibility(View.VISIBLE);
+                    }
+                });
+    }
+
+    private void sendFCMTokenToDatabase(String token) {
         FirebaseFirestore database = FirebaseFirestore.getInstance();
         DocumentReference documentReference =
                 database.collection(Constants.KEY_COLLECTION_USERS).document(
                         preferenceManager.getString(Constants.KEY_USER_ID)
                 );
         documentReference.update(Constants.KEY_FCM_TOKEN, token)
-                .addOnSuccessListener(aVoid -> Toast.makeText(MainActivity.this, "Token Updated Succcessfully", Toast.LENGTH_SHORT).show())
-                .addOnFailureListener(e -> Toast.makeText(MainActivity.this, "Unable To Send Token : "+e.getMessage(), Toast.LENGTH_SHORT).show());
+                .addOnFailureListener(e -> Toast.makeText(MainActivity.this, "Unable To Send Token : " + e.getMessage(), Toast.LENGTH_SHORT).show());
     }
 
-    private void signOut(){
+    private void signOut() {
         Toast.makeText(this, "Signing Out...", Toast.LENGTH_SHORT).show();
         FirebaseFirestore database = FirebaseFirestore.getInstance();
         DocumentReference documentReference =
